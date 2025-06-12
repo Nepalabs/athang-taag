c3a6fb0f5bc454dd2fc5e85a0d056fefa129b07c
Ugyen Choden <Ugyen Choden>
2025-06-12 10:54:43 +0600

    this is a authroute file

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const verifyAuth = require("../middlewares/verifyAuth.middleware");
+const authController = require("../controllers/auth.controller");
+const authRoutes = express.Router();
+
+authRoutes.get("/health", (req, res) => {
+  res.send(`Auth routes is running healthy`);
+});
+
+authRoutes.post("/signin", authController.signIn);
+
+authRoutes.post("/signup", authController.signUp);
+
+authRoutes.delete("/signout", verifyAuth, authController.signOut);
+
+authRoutes.get("/loggedin-user", verifyAuth, authController.getLoggedInUser);
+
+module.exports = authRoutes;
